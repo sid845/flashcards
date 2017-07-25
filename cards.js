23c9c842e5c988eb2f4accc62b73a5268e1d506f
Siddharth Patel <siddharthpatel845@gmail.com>
2017-07-25 15:29:58 -0400

    commit

--- cards.js
+++ cards.js
@@ -1,6 +1,6 @@
 exports.ClozeCard = function(full, cloze){
 	this.full = full;
 	this.cloze = cloze;
-	this.partial = full.replace(cloze, "_______");
+	this.partial = full.replace(cloze, "__________");
 }; 
 
